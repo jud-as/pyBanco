5b5e78c169d107545b1eb60563de1d6a1c9ec1bc
jud-as <guilhermeferreira082@gmail.com>
2024-11-06 23:53:15 -0300

    consertado bug de conta poupanca

--- application/main.py
+++ application/main.py
@@ -4,10 +4,12 @@ from domain.ContaCorrente import *
 from domain.ContaPoupanca import *
 
 def main():
+
     # Exemplo de uso
     cliente = Cliente("João","Silva" ,12345678900)
     conta1 = ContaCorrente(1, 1001, "Corrente", 0, 1000)
-    conta2 = ContaPoupanca(0, 1002, "Poupanca", 0, 0)
+    conta2 = ContaPoupanca("23/03", 1002, "Poupanca", 0, 0)
+
 
     # Verificando a associação
     print(f"Contas de {cliente.nome}: {cliente.listar_contas()}")
